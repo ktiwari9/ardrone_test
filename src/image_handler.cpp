ee8eaa9a5aea9e888d12ebb4647efcc13ab22d44
nicolasp94 <nicolas.castro.p94@gmail.com>
2016-08-31 12:02:07 -0300

    Camera in OpenCV window and timestamp on console

--- src/image_handler.cpp
+++ src/image_handler.cpp
@@ -1,16 +1,39 @@
 #include <ros/ros.h>
 #include "std_msgs/String.h"
 #include "ardrone_autonomy/Navdata.h"
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
 #include <iostream>
-/**
- * This tutorial demonstrates simple receipt of messages over the ROS system.
- */
+
+
+void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+
+	cv::Mat img = cv_bridge::toCvShare(msg, "bgr8")->image;
+    std_msgs::Header h = msg->header;
+    std::cout << h << std::endl;
+  	try
+  	{
+  	  cv::imshow("view", img);
+  	  cv::waitKey(30);
+  	}
+  	catch (cv_bridge::Exception& e)
+  	{
+  	  ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+  	}
+}
+
 
 int main(int argc, char **argv)
 {
 	std::cout << "Starting ARDRONE node" << std::endl;
 	ros::init(argc,argv, "main");
 	ros::NodeHandle n;
-	ros::spin();
-	return 0;
+  	cv::namedWindow("view");
+  	cv::startWindowThread();
+  	image_transport::ImageTransport it(n);
+  	image_transport::Subscriber sub = it.subscribe("ardrone/image_raw", 1, imageCallback);
+  	ros::spin();
+  	cv::destroyWindow("view");
 }
